eb7b970ae84e763f914228ff83db240c508a0979
Xage1 <kennethkiarie555@gmail.com>
2023-04-25 06:21:37 -0700

    Update

--- main.h
+++ main.h
@@ -2,33 +2,35 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdlib.h>
 
+int _printf(const char *format, ...);
 /**
- * struct MyPrint - Data type of a format.
- * @format: Format.
- * @func: Function.
+ * struct format - Data type of a format.
+ * @op: Format.
+ * @f: Function.
+ *
  */
 
-typedef struct MyPrint
+typedef struct format
 {
-char *format;
-void (*func)(va_list);
+char *op;
+int (*f)(va_list print);
 } MyPrint;
 
+
 int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_int(va_list args);
-int print_unsigned_int(va_list args);
-int print_binary(va_list args);
-int print_hex(va_list args);
-int print_octal(va_list args);
-int print_pointer(va_list args);
-int print_string_hex(va_list args);
-int print_reverse(va_list args);
-int print_rot13(va_list args);
+int op_character(va_list form);
+int op_string(va_list form);
+int op_integer(va_list form);
+int op_reverse(va_list form);
+int op_rot13(va_list form);
 int validator(const char *format, va_list print1, MyPrint *ops1);
+int op_binary(va_list form);
+int op_octal(va_list form);
+int op_unsigned_decimal(va_list form);
+int op_hex(va_list form);
+int op_HEX(va_list form);
+int op_SString(va_list form);
+int op_address(va_list form);
 
 #endif
